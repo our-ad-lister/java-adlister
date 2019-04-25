58b443d3d631cd1015b66410d5c81b92c6e47d0e
Cerissa Antill <cerissa.antill@gmail.com>
2019-04-25 11:52:32 -0500

    created DeleteAdServlet, EditAdServlet, & editAd.jsp

---
+++ src/main/java/com/codeup/adlister/controllers/DeleteAdServlet.java
@@ -0,0 +1,27 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = "/deleteAd")
+public class DeleteAdServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String hiddenParam = req.getParameter("selectedAdDelete");
+        long id = Long.parseLong(hiddenParam);
+        DaoFactory.getAdsDao().deleteEntry(id, 1);
+        DaoFactory.getAdsDao().deleteEntry(id, 2);
+        resp.sendRedirect("/profile");
+    }
+}
+
+
+
